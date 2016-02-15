60e5071275b11fbc4e421dfe6d8fee20a1a5c56e
Jill Tankersley <jillian.emilie@gmail.com>
2016-02-14 20:33:03 -0500

    ajax working w/appointments

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   resources :appointments
-  get 'welcome/index'
-
-  resources :welcomes
   resources :patients
   resources :doctors
 
-  root 'welcome#index'
+  root 'appointments#index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
